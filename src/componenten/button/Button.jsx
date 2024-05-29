1c369e6d0b0bdc5e59d8cdd5eaac89078460bf99
MisterVerhoef <frankverhoef@outlook.com>
2024-05-29 18:29:05 +0200

    made components and have begun integrating Fruitcounter

---
+++ src/componenten/button/Button.jsx
@@ -0,0 +1,16 @@
+
+import './Button.css';
+
+// eslint-disable-next-line react/prop-types
+function Fruitcounter({ title, counter, increase, decrease }) {
+    return (
+        <section>
+            <h2>{title}</h2>
+            <button type="button" onClick={decrease}>-</button>
+            <p>{counter}</p>
+            <button type="button" onClick={increase}>+</button>
+        </section>
+    );
+}
+
+export default Fruitcounter;
---
+++ src/componenten/form/Form.jsx
@@ -0,0 +1,36 @@
+import 'Form.css'
+
+function OrderForm() {
+
+    return (
+
+
+        <form onSubmit={handleSubmit}>
+
+            Voornaam <input type="text"/>
+            Achternaam <input type="text"/>
+            Leeftijd <input type="number" min="18" max="120"/>
+            Postcode <input type="text" maxLength={6}/>
+
+            Bezorgfrequentie
+            <select name="bezorgfrequentie" id="bezorgfrequentie">
+                <option value="dagelijks">Dagelijks</option>
+                <option value="wekelijks">Iedere week</option>
+                <option value="maandelijks">Iedere maand</option>
+            </select>
+
+            <input type="radio" name="overdag" value="overdag"/> Overdag
+            <input type="radio" name="avond" value="avond"/> Avond
+
+            Opmerkingen
+            <textarea name="opmerkingen" id="" cols="30" rows="10"></textarea>
+
+            <input type="checkbox" name="userTerms" value="agreedToUserTerms"/> Ik ga akkoord met
+            de voorwaarden
+            <button type="submit">Verzend</button>
+        </form>
+    )
+
+}
+
+export default OrderForm()
